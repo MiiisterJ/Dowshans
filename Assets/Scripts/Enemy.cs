5a7df9ef539b2179021dc9582fc8ee4ecca02c8c
Max S <bluegum_berry@hotmail.com>
2020-07-16 20:08:14 -0400

    Added Basic Enemy GameObject script
    New Enemy script for Enemy based game objects. Doesn't function as a collision system, rather as a stat container. More will be added soon.
    
    Also added Attack based statistics to the player characters. (Some descriptions too)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,13 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+
+#region Read Only Inspector
+public class ReadOnlyAttribute : PropertyAttribute
+{
+
+}
+
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property,
+                                            GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+
+    public override void OnGUI(Rect position,
+                               SerializedProperty property,
+                               GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
+#endregion
 
 public class Enemy : MonoBehaviour
 {
+    EnemyStats Monster;
+    [ReadOnly]
+    [SerializeField]
+    string Name;
+    public TextMesh TextTest;
+
+    public int MonsterID;
+    public bool Boss;
+    public int Level;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Monster = new EnemyStats(MonsterID, Boss, Level);
+        Name = Monster.Name;
+        TextTest.text = Name + " lvl. " + Monster.Level;
     }
 
     // Update is called once per frame
--- Assets/Scripts/PartyMember.cs
+++ Assets/Scripts/PartyMember.cs
@@ -10,7 +10,6 @@ using UnityEngine;
 /// </summary>
 public class PartyMember : MonoBehaviour
 {
-    string charName;
     public string Name { get; private set; }
 
     
@@ -30,10 +29,17 @@ public class PartyMember : MonoBehaviour
     int baseMaxExp = 100;
     const int MAX_LEVEL = 100;
 
+    double AtkMultiplier; //Used when a character levels up.
+    int baseAtk;
+    public int Stat_Attack { get; private set; }
+
+
     public PartyMember(int _memberID, int _exp)
     {
         Character(_memberID);
         Exp = _exp;
+
+        //Set maximum stats.
         UpdateStats();
     }
 
@@ -54,6 +60,10 @@ public class PartyMember : MonoBehaviour
         maxMana = Convert.ToInt32(baseMana * (Level * ManaMultiplier));
     }
 
+    /// <summary>
+    /// Profile of party members you have.
+    /// </summary>
+    /// <param name="_IDnum">Profile ID number.</param>
     private void Character(int _IDnum)
     {
         switch (_IDnum)
@@ -62,29 +72,41 @@ public class PartyMember : MonoBehaviour
                 Name = "Stanley";
                 baseHP = 100;
                 baseMana = 100;
+                baseAtk = 10;
+
                 HPMultiplier = 1.1;
                 ManaMultiplier = 0.8;
+                AtkMultiplier = 1.5;
                 break;
             case 2:
                 Name = "Groose";
                 baseHP = 100;
                 baseMana = 100;
+                baseAtk = 10;
+
                 HPMultiplier = 1.0;
                 ManaMultiplier = 1.0;
+                AtkMultiplier = 1.25;
                 break;
             case 3:
                 Name = "GT Goku";
                 baseHP = 50;
                 baseMana = 100;
-                HPMultiplier = 1.5;
+                baseAtk = 15;
+
+                HPMultiplier = 0.5;
                 ManaMultiplier = 1.2;
+                AtkMultiplier = 1.2;
                 break;
             case 4:
                 Name = "Side-B Charizard";
                 baseHP = 130;
                 baseMana = 50;
+                baseAtk = 5;
+
                 HPMultiplier = 1.25;
                 ManaMultiplier = 0.5;
+                AtkMultiplier = 2.0;
                 break;
         }
     }
