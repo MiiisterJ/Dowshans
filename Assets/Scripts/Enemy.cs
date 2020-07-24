98fe86bfca3a36f2cc0781e7de62ab81f4c7d53c
Max S <bluegum_berry@hotmail.com>
2020-07-23 22:29:46 -0400

    Big code and scene overhaul
    -Fixed Jumping in Controls.
    - Added Ground tag to objects that you run on with the player. This makes it so it can only jump when landing on appropriate ground objects. eg platforms, rock formations, etc.
    - Reassessed how party members are initialized. Created empty GameObjects containing each party member information, linked along with the 'Game Logic' object.
    - Moved [ReadOnly] attribute to GameLogic script.
    - Party members should always be set to level 1, unless given experience points to be at appropriate level.
    - Adjusted UI canvas naming. Game logic can now update info on canvas objects. (only on Rocky Mountain scene currently)
    - Added UI to Battle Scene (Still WIP)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,40 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
-
-#region Read Only Inspector
-public class ReadOnlyAttribute : PropertyAttribute
-{
-
-}
-
-
-[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
-public class ReadOnlyDrawer : PropertyDrawer
-{
-    public override float GetPropertyHeight(SerializedProperty property,
-                                            GUIContent label)
-    {
-        return EditorGUI.GetPropertyHeight(property, label, true);
-    }
-
-    public override void OnGUI(Rect position,
-                               SerializedProperty property,
-                               GUIContent label)
-    {
-        GUI.enabled = false;
-        EditorGUI.PropertyField(position, property, label, true);
-        GUI.enabled = true;
-    }
-}
-#endregion
 
 public class Enemy : MonoBehaviour
 {
     EnemyStats Monster;
-    [ReadOnly]
-    [SerializeField]
+    [ReadOnly] [SerializeField]
     string Name;
     public TextMesh TextTest;
 
--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,26 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+#region Read Only Inspector
+public class ReadOnlyAttribute : PropertyAttribute
+{
+
+}
+
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property,
+                                            GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+
+    public override void OnGUI(Rect position,
+                               SerializedProperty property,
+                               GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
+#endregion
 
 public class GameLogic : MonoBehaviour
 {
-    PartyMember[] Character = new PartyMember[4]; // In the box, put numbers from 0 to 3.
+    public PartyMember[] Character = new PartyMember[4]; // In the box, put numbers from 0 to 3.
+
+    int GameMode;
+
+    [ReadOnly] [SerializeField]
+    int testOutput;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        Character[0] = new PartyMember(1, 0);
+        UI_Update();
+        testOutput++;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //UI_Update();
+        //testOutput++;
     }
 
     void LoadFile_ExperiencePoints()
     {
 
+    }
+
+    void UI_Update()
+    {
+        Text UITextBox;
+        for (int i = 0; i < 4; i++)
+        {
+            switch (Character[i].MemberID)
+            {
+                case 1:
+                    UITextBox = GameObject.Find("Rocky UI").GetComponent<Text>();
+                    UITextBox.text = Character[i].Name + " Lvl. " + Character[i].Level;
+                    break;
+                case 2:
+                    UITextBox = GameObject.Find("Luke UI").GetComponent<Text>();
+                    UITextBox.text = Character[i].Name + " Lvl. " + Character[i].Level;
+                    break;
+                case 3:
+                    UITextBox = GameObject.Find("Jane UI").GetComponent<Text>();
+                    UITextBox.text = Character[i].Name + " Lvl. " + Character[i].Level;
+                    break;
+                case 4:
+                    UITextBox = GameObject.Find("Phoenix UI").GetComponent<Text>();
+                    UITextBox.text = Character[i].Name + " Lvl. " + Character[i].Level;
+                    break;
+            }
+        }
     }
 }
